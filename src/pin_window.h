a6498c730654b4625b40a9ccfca0c8185ea7277c
Elias不吃糖 <qskaouayshuayszas@gmail.com>
2026-08-20 09:49:06 +0800

    Add pin window interface

---
+++ src/pin_window.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <windows.h>
+
+namespace snaplite {
+
+class PinWindow {
+public:
+    static bool Register(HINSTANCE instance);
+    static bool Create(HINSTANCE instance, HBITMAP bitmap);
+    static bool CreateFromClipboard(HINSTANCE instance);
+
+private:
+    PinWindow(HINSTANCE instance, HBITMAP bitmap);
+    ~PinWindow();
+
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+    LRESULT HandleMessage(UINT message, WPARAM wParam, LPARAM lParam);
+
+    void ResizeForZoom();
+    void AdjustZoom(int wheelDelta);
+    void AdjustOpacity(int wheelDelta);
+    void ShowContextMenu();
+
+    HINSTANCE instance_{};
+    HWND hwnd_{};
+    HBITMAP bitmap_{};
+    int bitmapWidth_{};
+    int bitmapHeight_{};
+    double zoom_{1.0};
+    BYTE opacity_{255};
+};
+
+}  // namespace snaplite
