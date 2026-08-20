0821ed7b915b40158dccbbc77e3f4f53b4e630e2
Elias不吃糖 <qskaouayshuayszas@gmail.com>
2026-08-20 09:50:27 +0800

    Add application shell interface

---
+++ src/app.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <windows.h>
+#include <shellapi.h>
+
+#include <gdiplus.h>
+
+namespace snaplite {
+
+class App {
+public:
+    explicit App(HINSTANCE instance);
+    ~App();
+
+    int Run(int showCommand);
+
+private:
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+    LRESULT HandleMessage(UINT message, WPARAM wParam, LPARAM lParam);
+
+    bool Initialize();
+    void Shutdown();
+    void AddTrayIcon();
+    void ShowTrayMenu();
+    void ShowNotice(const wchar_t* message);
+    void StartSnip();
+    void CaptureFullscreen();
+    void PinClipboard();
+    void CommitCapture(HBITMAP bitmap);
+
+    HINSTANCE instance_{};
+    HWND hwnd_{};
+    NOTIFYICONDATAW tray_{};
+    ULONG_PTR gdiplusToken_{};
+    HANDLE singleInstance_{};
+};
+
+}  // namespace snaplite
